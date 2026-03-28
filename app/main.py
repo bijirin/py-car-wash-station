d229ee1ea536763dd7480fec6720efde8a07d2dd
bijirin <tsutrisn@gmail.com>
2026-03-28 18:00:10 +0700

    Refactor type hints for clarity and consistency in Car and CarWashStation classes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 class Car:
     def __init__(self,
-                 comfort_class:int,
+                 comfort_class: int,
                  clean_mark: int,
-                 brand: str):
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -13,19 +13,19 @@ class CarWashStation:
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
-                 count_of_ratings: int):
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0.0
         for car in cars:
             income += self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         washing_price = 0.0
         if self.clean_power > car.clean_mark:
             washing_price = car.comfort_class \
@@ -34,14 +34,14 @@ class CarWashStation:
                 / self.distance_from_city_center
         return round(washing_price, 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> float:
         cost = 0.0
         if self.clean_power > car.clean_mark:
             cost = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
         return cost
 
-    def rate_service(self, rating: float):
+    def rate_service(self, rating: float) -> None:
         current_sum_rating = self.average_rating * self.count_of_ratings
         new_sum_rating = current_sum_rating + rating
         self.count_of_ratings += 1
