85fe65b1ff0751e62ea2078b40daca0353ccfed6
bijirin <tsutrisn@gmail.com>
2026-03-28 17:55:01 +0700

    Implement Car and CarWashStation classes with core functionality

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class:int,
+                 clean_mark: int,
+                 brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list):
+        income = 0.0
+        for car in cars:
+            income += self.wash_single_car(car)
+        return income
+
+    def calculate_washing_price(self, car: Car):
+        washing_price = 0.0
+        if self.clean_power > car.clean_mark:
+            washing_price = car.comfort_class \
+                * (self.clean_power - car.clean_mark) \
+                * self.average_rating \
+                / self.distance_from_city_center
+        return round(washing_price, 1)
+
+    def wash_single_car(self, car: Car):
+        cost = 0.0
+        if self.clean_power > car.clean_mark:
+            cost = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+        return cost
+
+    def rate_service(self, rating: float):
+        current_sum_rating = self.average_rating * self.count_of_ratings
+        new_sum_rating = current_sum_rating + rating
+        self.count_of_ratings += 1
+        self.average_rating = round(new_sum_rating / self.count_of_ratings, 1)
